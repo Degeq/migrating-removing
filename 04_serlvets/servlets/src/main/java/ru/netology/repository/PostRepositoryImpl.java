6657acc3ddfdb7ffc20825270dab4ae44e3d8e1c
Degeq <gorlovoy89@mail.ru>
2023-10-14 16:56:53 +0300

    HW-04 Annotation Config

---
+++ 04_serlvets/servlets/src/main/java/ru/netology/repository/PostRepositoryImpl.java
@@ -0,0 +1,48 @@
+package ru.netology.repository;
+
+import org.springframework.stereotype.Repository;
+import ru.netology.exception.NotFoundException;
+import ru.netology.model.Post;
+
+import java.util.*;
+import java.util.concurrent.atomic.AtomicInteger;
+
+@Repository
+public class PostRepositoryImpl implements PostRepository{
+
+    private HashMap<Long, Post> postContainer = new HashMap<>();
+    private AtomicInteger postCounter = new AtomicInteger(1);
+
+    public List<Post> all() {
+        return new ArrayList<>(postContainer.values());
+    }
+
+    public Optional<Post> getById(long id) {
+        return Optional.ofNullable(postContainer.get(id));
+    }
+
+    public Post save(Post post) {
+        if (post.getId() == 0) {
+            synchronized (postCounter) {
+                post.setId(postCounter.get());
+                postContainer.put(Long.valueOf(postCounter.getAndIncrement()), post);
+            }
+        } else {
+            if (postContainer.containsKey(post.getId())) {
+                postContainer.put(post.getId(), post);
+            } else {
+                throw new NotFoundException("Пост с таким ID не найден");
+            }
+        }
+        return post;
+    }
+
+
+    public void removeById(long id) {
+        if (postContainer.containsKey(id)) {
+            postContainer.remove(id);
+        } else {
+            throw new NotFoundException("Пост с таким ID не найден");
+        }
+    }
+}
